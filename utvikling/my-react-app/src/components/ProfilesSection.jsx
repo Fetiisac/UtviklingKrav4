b08a0d23c863d8876c2c7f98aa374848c48ccbbb
Andara2 <maandara@hiof.no>
2026-04-28 23:03:11 +0200

    kort og css

--- utvikling/my-react-app/src/components/ProfilesSection.jsx
+++ utvikling/my-react-app/src/components/ProfilesSection.jsx
@@ -1,8 +1,12 @@
+import FetiProfile from './profiler/FetiProfile'
+import MarcoProfile from './profiler/MarcoProfile'
+
 export default function ProfilesSection() {
   return (
     <section aria-labelledby="group-members">
       <h2 id="group-members">Gruppemedlemmer</h2>
-      <p>Her skal gruppemedlemmenes profilkomponenter legges inn gjennom egne branches.</p>
+      <FetiProfile />
+      <MarcoProfile />
     </section>
   )
 }
---
+++ utvikling/my-react-app/src/components/profiller/Marco-kort.jsx
@@ -0,0 +1,15 @@
+export default function MarcoProfile() {
+  return (
+    <article>
+      <img
+        src="https://via.placeholder.com/200x200.png?text=Marco"
+        alt="Illustrasjon av Marco"
+      />
+      <h3>Marco</h3>
+      <p>
+        <a href="mailto:maandara@hiof.no">maandara@hiof.no</a>
+      </p>
+      <p>Bachelorstudie her</p>
+    </article>
+  )
+}
